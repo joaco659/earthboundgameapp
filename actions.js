e61a1f136e7155edf94503751120f78dd16edd1b
joaco659 <joaco659782@gmail.com>
2024-03-16 20:43:43 -0300

    major refactor on everything, psi attack bases added. few critical bugs need to be fixed

--- actions.js
+++ actions.js
@@ -1,12 +1,37 @@
 export const actions = {
   getBashEnemyDamage: (enemy, attackUsed, playerStats) => {
+    // Cosas por añadir
+    // Refactorizar chance del SMAAAASH, y que despues de que haga no se saltee las probabilidades de abajo:
+
+    // Chance de fallar el ataque
+    // Si el jugador tiene el estado de efecto de llorar, aumentara su chance de fallar el ataque
+    if (
+      playerStats.statusEffects["crying"] ||
+      playerStats.statusEffects["nausea"]
+    ) {
+      const missRate = Math.floor(Math.random() * 16) + 8;
+      if (missRate == 8) return [0, { smashed: false, missed: true }];
+    } else {
+      const missRate = Math.floor(Math.random() * 16) + 1;
+      if (missRate == 1) return [0, { smashed: false, missed: true }];
+    }
+
     // Ataques criticos (los SMAAAASHes!)
-    // Duplican el daño infligo al enemigo
-    const smashed =
-      Math.round(Math.random() * playerStats.guts) == playerStats.guts;
+    // Duplican el daño infligido al enemigo
+    const smashChance = Math.max(playerStats.guts / 500, 1 / 20);
+    const smashed = Math.random() < smashChance;
     const attackDamage = smashed
-      ? (attackUsed.attackLevel * playerStats.offense - enemy.stats.defense) * 2
+      ? 4 * playerStats.offense - enemy.stats.defense
       : attackUsed.attackLevel * playerStats.offense - enemy.stats.defense;
+
+    // Chance de que los enemigos esquiven el ataque
+    // Si el jugador hace un ataque critico el enemigo no podra esquivarlo
+    if (!smashed) {
+      const enemyDodgeRate =
+        (2 * (enemy.stats.speed - playerStats.speed)) / 500;
+      const enemyDodged = Math.random() < enemyDodgeRate;
+      if (enemyDodged) return [0, { smashed: false, enemyDodged: true }];
+    }
 
     // Factor benefactor para el jugador
     // Cada ataque tiene chance de infligir 20% mas o menos de daño al enemigo.
@@ -20,6 +45,8 @@ export const actions = {
       (attackDamage * 20) / 100,
     ];
 
+    // Debug
+    console.groupCollapsed("Daños");
     console.log("Daño infligido al enemigo: ", attackDamage);
     if (smashed) {
       console.log("SMAAAASH!");
@@ -30,7 +57,88 @@ export const actions = {
       "Ahora la vida del enemigo es: ",
       enemy.stats.hp - Math.abs(attackDamage + extraDamage[rng])
     );
+    console.groupEnd();
 
-    return [Math.abs(attackDamage + extraDamage[rng]), smashed];
+    return [
+      Math.round(Math.abs(attackDamage + extraDamage[rng])),
+      { smashed: smashed },
+    ];
+  },
+  getPluralPsiMoveAction: (enemies, psiMove, playerStats) => {
+    // Implementar logica para los ataques psi
+    const PSI_ATTACKS_DICT = {
+      psi_rockin: {
+        alpha: () => {
+          const attackDamage = 40 + Math.round(Math.random() * 80);
+
+          const psiResults = [];
+          enemies.forEach((enemy) => {
+            const missRate = (2 * enemy.stats.speed - playerStats.speed) / 10;
+
+            console.log("MISS RATE ", missRate);
+
+            const enemyDodged = Math.random() < 0;
+            enemyDodged
+              ? psiResults.push({
+                  to: enemy,
+                  results: [0, false, { enemyDodged: true }],
+                })
+              : psiResults.push({
+                  to: enemy,
+                  results: [attackDamage, false, {}],
+                });
+          });
+          return psiResults;
+        },
+      },
+    };
+
+    const psiResult =
+      PSI_ATTACKS_DICT[psiMove.psiName][psiMove.psiSelectedLevel]();
+
+    console.log(
+      `RESULTADO DEL PSI: ${psiMove.psiName} ${psiMove.psiSelectedLevel}`,
+      psiResult
+    );
+
+    return psiResult;
+  },
+  getSinglePsiMoveAction: (enemy, psiMove, playerStats) => {
+    // Implementar logica para los ataques psi
+    const PSI_ATTACKS_DICT = {
+      psi_freeze: {
+        alpha: () => {
+          const freezeChance = 0.1;
+          const attackDamage = 30 + Math.round(Math.random() * 90);
+          // Si el enemigo tiene debilidad al hielo, le hara un 25% mas de daño
+          enemy.stats.vulnerabilities.forEach((vulnerability) => {
+            if (vulnerability == psiMove.psiName)
+              return Math.round(Math.abs(attackDamage + (attackDamage * 25) / 100));
+          });
+          // Si el enemigo tiene resistencia al hielo, le hara un 25% menos de daño
+          enemy.stats.strengths.forEach((strength) => {
+            if (strength == psiMove.psiName)
+              return Math.round(Math.abs(attackDamage - (attackDamage * 25) / 100));
+          });
+
+          // Daño mas random
+          if (Math.random() < 0.5) {
+            return Math.round(Math.abs(attackDamage - (attackDamage * 10) / 100));
+          } else {
+            return Math.round(Math.abs(attackDamage + (attackDamage * 10) / 100));
+          }
+        },
+      },
+    };
+
+    const psiResult =
+      PSI_ATTACKS_DICT[psiMove.psiName][psiMove.psiSelectedLevel]();
+
+    console.log(
+      `RESULTADO DEL PSI: ${psiMove.psiName} ${psiMove.psiSelectedLevel}`,
+      psiResult
+    );
+
+    return psiResult;
   },
 };
--- main.js
+++ main.js
@@ -4,6 +4,15 @@ import { actions } from "./actions.js";
 
 // Funcion utilitaria
 const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+const clearAllChildren = (element) => {
+  while (element.firstChild) {
+    element.removeChild(element.lastChild);
+  }
+};
+
+// 0: opcion nueva
+// 1: opcion vieja
+let mainUiOptionSelected = null;
 
 const hpCounter = new RollingCounter(
   document.getElementById("hp-counter"),
@@ -48,6 +57,9 @@ const SFX = {
   smash: new Howl({
     src: ["./sounds/smash.wav"],
   }),
+  enemyHit: new Howl({
+    src: ["./sounds/enemyhit.wav"],
+  }),
 };
 
 // Efecto de maquina de escribir como en el juego original
@@ -85,16 +97,28 @@ const blinkBgColor = async (color) => {
   await wait(300);
   bgScreenColor.classList.add("hidden");
 };
+const blinkEnemySprite = async (enemySprite) => {
+  enemySprite.classList.add("invisible");
+  await wait(200);
+  enemySprite.classList.remove("invisible");
+  await wait(200);
+  enemySprite.classList.add("invisible");
+  await wait(200);
+  enemySprite.classList.remove("invisible");
+};
 
 const charSelectMenu = document.createElement("div");
 charSelectMenu.classList.add("charselect-menu");
 
 let actionSelected = null;
-const displayCharacterSelector = (context, contextElement, characters) => {
+const displayCharacterSelector = (
+  context,
+  contextElement,
+  characters,
+  psiMove = null
+) => {
   // Borrar enemigos anteriores
-  while (charSelectMenu.firstChild) {
-    charSelectMenu.removeChild(charSelectMenu.lastChild);
-  }
+  clearAllChildren(charSelectMenu);
 
   if (actionSelected == contextElement) {
     charSelectMenu.remove();
@@ -109,44 +133,93 @@ const displayCharacterSelector = (context, contextElement, characters) => {
     console.log(character);
     const characterLI = document.createElement("li");
     characterLI.textContent = character.name;
-    characterLI.addEventListener("click", () => {
-      console.log("Enemigo ", character.name, "seleccionado.");
-      if (context.toLowerCase() == "enemies") {
-        console.log("vida de antes del enemigo: ", character.stats.hp);
-        const [attackDamage, playerSmashed] = actions.getBashEnemyDamage(
-          character,
-          PLAYER_BASH_ATTACK,
-          PLAYER_STATS
-        );
-        toggleDialogueBox();
-        SFX.attack1.play();
-        SFX.attack1.once("end", async () => {
-          await wait(200);
 
-          if (playerSmashed) {
-            const smashImage = document.getElementById("smash-attack");
-            smashImage.classList.toggle("hidden");
-            SFX.smash.play();
-            blinkBgColor("#00ff6a99");
+    if (contextElement == actionButtons.bash) {
+      // Ataque comun
+      characterLI.addEventListener("click", () => {
+        console.log("Enemigo ", character.name, "seleccionado.");
+        if (context.toLowerCase() == "enemies") {
+          console.log("vida de antes del enemigo: ", character.stats.hp);
+          const [attackDamage, additionalAttackInfo] =
+            actions.getBashEnemyDamage(
+              character,
+              PLAYER_BASH_ATTACK,
+              PLAYER_STATS
+            );
+          toggleDialogueBox();
+          SFX.attack1.play();
+          SFX.attack1.once("end", async () => {
+            await wait(200);
 
-            SFX.smash.once("end", async () => {
-              await typewrite(
-                `${PLAYER_BASH_ATTACK.attackMessage} ${character.name},\r\n infligiendo ${attackDamage} de daño.`
-              );
-            });
-          } else {
-            SFX.attack2.play();
-            SFX.attack2.once("end", async () => {
-              await typewrite(
-                `${PLAYER_BASH_ATTACK.attackMessage} ${character.name},\r\n infligiendo ${attackDamage} de daño.`
-              );
-            });
-          }
-        });
-      }
-      charSelectMenu.remove();
-      actionSelected = null;
-    });
+            if (additionalAttackInfo.missed) {
+              await typewrite("Fallo el ataque!");
+            } else if (additionalAttackInfo.smashed) {
+              const smashImage = document.getElementById("smash-attack");
+              smashImage.classList.toggle("hidden");
+              SFX.smash.play();
+              blinkEnemySprite(character.sprite);
+              blinkBgColor("#00ff6a99");
+
+              SFX.smash.once("end", async () => {
+                // SFX.enemyHit.play();
+                await typewrite(
+                  `${PLAYER_BASH_ATTACK.attackMessage} ${character.name},\r\n infligiendo ${attackDamage} de daño.`
+                );
+              });
+            } else {
+              SFX.attack2.play();
+              SFX.attack2.once("end", async () => {
+                if (additionalAttackInfo.enemyDodged) {
+                  await typewrite("El enemigo esquivo el ataque!");
+                } else {
+                  SFX.enemyHit.play();
+                  blinkEnemySprite(character.sprite);
+                  await typewrite(
+                    `${PLAYER_BASH_ATTACK.attackMessage} ${character.name},\r\n infligiendo ${attackDamage} de daño.`
+                  );
+                }
+              });
+            }
+          });
+        }
+        charSelectMenu.remove();
+        actionSelected = null;
+      });
+    } else if (contextElement == actionButtons.psi && psiMove) {
+      // Con movimiento psi seleccionado
+      const psiMoveSelectedContainer = document.createElement("ul");
+      psiMoveSelectedContainer.classList.add("charselect-menu--selected-move");
+
+      const psiMoveSelectedLabel = document.createElement("li");
+      psiMoveSelectedLabel.textContent = psiMove.psiDisplayName;
+      psiMoveSelectedContainer.appendChild(psiMoveSelectedLabel);
+
+      charSelectMenu.appendChild(psiMoveSelectedContainer);
+
+      characterLI.addEventListener("click", () => {
+        console.log("Personaje", character.name, "seleccionado.");
+        // Ataques que van dirigidos al enemigo
+        // (Hay que calcular si hace daño o aplica un estado de efecto)
+        if (context.toLowerCase() == "enemies") {
+          // console.log("vida de antes del enemigo: ", character.stats.hp);
+
+          toggleDialogueBox();
+          SFX.attack1.play();
+          SFX.attack1.once("end", async () => {
+            await wait(200);
+            const psiResults = actions.getSinglePsiMoveAction(
+              character,
+              psiMove,
+              PLAYER_STATS
+            );
+
+            await typewrite("Ataque psi");
+          });
+        }
+        charSelectMenu.remove();
+        actionSelected = null;
+      });
+    }
 
     charSelectMenu.appendChild(characterLI);
   });
@@ -157,6 +230,219 @@ const displayCharacterSelector = (context, contextElement, characters) => {
 const toggleDialogueBox = () => {
   document.querySelector(".actions__inner").classList.toggle("hidden");
   document.querySelector(".dialogue-box").classList.toggle("hidden");
+};
+
+// Setup del menu de PSI
+const psiSelectMenu = document.createElement("div");
+psiSelectMenu.classList.add("psiselect-menu");
+// Funcion multiuso para renderizar interfaz fija y dinamica (los ataques PSI)
+const psiSelectMenuBody = {
+  info: () => {
+    const psiInfoContainer = document.createElement("ul");
+    psiInfoContainer.classList.add("psiselect-menu__psi-info");
+    psiInfoContainer.classList.add("hidden");
+    const psiInfoSubject = document.createElement("li");
+    const psiInfoPPCost = document.createElement("li");
+    psiInfoContainer.appendChild(psiInfoSubject);
+    psiInfoContainer.appendChild(psiInfoPPCost);
+
+    return psiInfoContainer;
+  },
+  types: () => {
+    const psiSelectMenuTypes = document.createElement("ul");
+    psiSelectMenuTypes.classList.add("psiselect-menu__psi-types");
+    const offense = document.createElement("li");
+    const recover = document.createElement("li");
+    const assist = document.createElement("li");
+    offense.textContent = "Offense";
+    offense.dataset.psiType = "offense";
+    recover.textContent = "Recover";
+    recover.dataset.psiType = "recover";
+    assist.textContent = "Assist";
+    assist.dataset.psiType = "assist";
+
+    const loadMoves = (opt) => {
+      offense.classList.remove("selected-opt");
+      recover.classList.remove("selected-opt");
+      assist.classList.remove("selected-opt");
+
+      clearAllChildren(psiSelectMenu.children[2]);
+      psiSelectMenuBody.moves(PLAYER_PSI_MOVES[opt]).forEach((move) => {
+        psiSelectMenu.children[2].appendChild(move);
+      });
+    };
+
+    // Carga de los movimientos
+    offense.addEventListener("click", (e) => {
+      loadMoves("offense");
+      offense.classList.add("selected-opt");
+    });
+
+    recover.addEventListener("click", (e) => {
+      loadMoves("recover");
+      recover.classList.add("selected-opt");
+    });
+
+    assist.addEventListener("click", (e) => {
+      loadMoves("assist");
+      assist.classList.add("selected-opt");
+    });
+
+    psiSelectMenuTypes.appendChild(offense);
+    psiSelectMenuTypes.appendChild(recover);
+    psiSelectMenuTypes.appendChild(assist);
+
+    return psiSelectMenuTypes;
+  },
+  movesContainer: () => {
+    const psiMovesContainer = document.createElement("ul");
+    psiMovesContainer.classList.add("psiselect-menu__psi-moves");
+    return psiMovesContainer;
+  },
+  moves: (PSI_MOVES) => {
+    const psiMovesContainer = [];
+    PSI_MOVES.map((psiMove) => {
+      const psiMoveContainer = document.createElement("ul");
+      psiMoveContainer.classList.add("psiselect-menu__psi-moves--psi-move");
+      psiMoveContainer.dataset.psiMoveName = psiMove.psiName;
+
+      const psiDisplayName = document.createElement("p");
+
+      const psiMoveLevelsContainer = document.createElement("ul");
+      psiMoveLevelsContainer.classList.add("psi-move--levels");
+
+      psiDisplayName.textContent = psiMove.psiDisplayName;
+      psiMoveContainer.appendChild(psiDisplayName);
+      psiMoveContainer.appendChild(psiMoveLevelsContainer);
+
+      const renderedPsiLevels = [];
+      const psiLevelsRenderList = [
+        ["alpha", "beta", "gamma", "Omega"],
+        ["alpha", "beta", "Sigma", "Omega"],
+      ];
+
+      Object.entries(psiMove.psiLevels).forEach((level) => {
+        if (!(level[0] == "psiLevelType")) {
+          const psiMoveLevel = document.createElement("li");
+          // textContent e innerText no aceptan entidades html (&alpha;), en su lugar uso innerHTML
+          psiMoveLevel.innerHTML = `&${level[0]};`;
+          psiMoveLevel.dataset.psiLevel = level[0];
+          psiMoveLevel.dataset.psiLevelEnabled = true;
+          renderedPsiLevels.push(level[0]);
+
+          // Muestra info del ataque psi como el costo y el alcance
+          psiMoveLevel.addEventListener("mouseenter", () => {
+            const psiInfoContainer = psiSelectMenu.children[0];
+            psiInfoContainer.classList.remove("hidden");
+            psiInfoContainer.children[0].textContent =
+              psiMove.psiSubjectMessage;
+            psiInfoContainer.children[1].textContent = `PP Cost: ${level[1].ppCost}`;
+          });
+
+          psiMoveLevel.addEventListener("click", () => {
+            let selectedPsiMove = psiMove;
+            selectedPsiMove.psiSelectedLevel = level[0];
+            psiSelectMenu.remove();
+
+            actionSelected = null;
+            if (selectedPsiMove.psiSubject == "enemies") {
+              // Cuando es un psi que afecta a todos los enemigos, no muestra el selector de personaje
+              if (selectedPsiMove.psiScope == "all") {
+                (async function () {
+                  const psiResults = actions.getPluralPsiMoveAction(
+                    Enemies,
+                    selectedPsiMove,
+                    PLAYER_STATS
+                  );
+
+                  toggleDialogueBox();
+                  await typewrite("Ataque psi en plural");
+                })();
+              } else {
+                displayCharacterSelector(
+                  "enemies",
+                  actionButtons.psi,
+                  Enemies,
+                  selectedPsiMove
+                );
+              }
+            } else {
+              displayCharacterSelector(
+                "allies",
+                actionButtons.psi,
+                // Refactorizar
+                // Hacer una variable donde se guarde el equipo, incluyendo al jugador
+                // (eso si en un futuro se pueden incluir aliados) :)
+                [{ name: "Jugador" }],
+                selectedPsiMove
+              );
+            }
+          });
+
+          psiMoveLevel.addEventListener("mouseleave", () => {
+            const psiInfoContainer = psiSelectMenu.children[0];
+            psiInfoContainer.classList.add("hidden");
+            psiInfoContainer.children[0].textContent = "";
+            psiInfoContainer.children[1].textContent = "";
+          });
+
+          psiMoveLevelsContainer.appendChild(psiMoveLevel);
+        }
+      });
+
+      // Debug
+      // console.log(
+      //   `Rendered psi levels for ${psiMove.psiDisplayName}:`,
+      //   renderedPsiLevels
+      // );
+
+      /*
+        Esto lo hago por imitar la interfaz del juego donde no aparecen los demas niveles,
+        pero su espacio vacio sigue ahi para cuando aparezcan.
+      */
+      psiLevelsRenderList[psiMove.psiLevels.psiLevelType].map(
+        (level, index) => {
+          if (!(level == renderedPsiLevels[index])) {
+            const psiMoveLevel = document.createElement("li");
+            psiMoveLevel.innerHTML = `&${level};`;
+            psiMoveLevel.dataset.psiLevel = level;
+            psiMoveLevel.dataset.psiLevelEnabled = false;
+            psiMoveLevelsContainer.appendChild(psiMoveLevel);
+          }
+        }
+      );
+
+      psiMovesContainer.push(psiMoveContainer);
+    });
+    return psiMovesContainer;
+  },
+};
+
+psiSelectMenu.appendChild(psiSelectMenuBody.info());
+psiSelectMenu.appendChild(psiSelectMenuBody.types());
+psiSelectMenu.appendChild(psiSelectMenuBody.movesContainer());
+
+const displayPsiSelector = (contextElement) => {
+  clearAllChildren(psiSelectMenu.children[2]);
+  if (actionSelected == contextElement) {
+    // Se quita el seleccionado de las demas opciones al abrir el menu
+    Object.entries(psiSelectMenu.children[1].children).forEach((key) =>
+      key[1].classList.remove("selected-opt")
+    );
+    psiSelectMenu.remove();
+    console.log("already selected");
+    actionSelected = null;
+    return;
+  }
+  actionSelected = contextElement;
+
+  contextElement.appendChild(psiSelectMenu);
+
+  // Se muestran los PSI ofensivos al abrir el menu
+  psiSelectMenu.children[1].children[0].classList.add("selected-opt");
+  psiSelectMenuBody.moves(PLAYER_PSI_MOVES.offense).forEach((move) => {
+    psiSelectMenu.children[2].appendChild(move);
+  });
 };
 
 const actionButtons = {
@@ -172,17 +458,19 @@ const Enemies = [
   {
     name: "Joaco",
     initialMessage: "El creador del fangame te reta a una batalla.",
+    sprite: document.getElementById("enemy"),
     stats: {
-      hp: 99,
+      hp: 800,
       pp: 0,
-      offense: 20,
-      defense: 25,
-      speed: 6,
-      guts: 50,
+      offense: 55,
+      defense: 31,
+      speed: 9,
+      guts: 14,
       rewardExp: 130,
       wealth: 659,
       drops: [],
-      vulnerabilities: ["psi_fire", "psi_freeze"],
+      vulnerabilities: ["psi_fire"],
+      strengths: ["psi_freeze"],
     },
     attacks: [
       {
@@ -196,13 +484,17 @@ const Enemies = [
   },
 ];
 
+// Mas o menos las stats que tiene Ness al nivel 19
+// (lo nerfee)
 const PLAYER_STATS = {
-  hp: 85,
-  pp: 25,
-  offense: 15,
-  defense: 20,
-  speed: 5,
-  guts: 30,
+  hp: 165,
+  pp: 30,
+  offense: 45,
+  defense: 24,
+  speed: 11,
+  luck: 13,
+  guts: 15,
+  statusEffects: [],
 };
 
 const PLAYER_BASH_ATTACK = {
@@ -213,26 +505,77 @@ const PLAYER_BASH_ATTACK = {
   ppCost: 0,
 };
 
+// Los ataques psi deberian ir todos en un archivo JSON a modo de DB
+// (tambien estan nerfeados xd)
+const PLAYER_PSI_MOVES = {
+  offense: [
+    {
+      psiName: "psi_rockin",
+      psiDisplayName: "PSI Rockin",
+      psiSubject: "enemies",
+      psiScope: "all",
+      psiSubjectMessage: "To all the enemies",
+      psiLevels: {
+        // Esto es para diferenciar si los niveles deben renderizarse de distinta forma
+        // -- Averiguar una forma de optimizar
+        psiLevelType: 0,
+        alpha: {
+          damage: [50, 100],
+          ppCost: 10,
+        },
+      },
+    },
+    {
+      psiName: "psi_freeze",
+      psiDisplayName: "PSI Freeze",
+      psiSubject: "enemies",
+      // Tiene de scope a todos los enemigos, pero selecciona varios o uno solo al azar
+      psiScope: "one",
+      psiSubjectMessage: "To one enemy",
+      psiLevels: {
+        psiLevelType: 0,
+        alpha: {
+          damage: [60, 120],
+          ppCost: 4,
+        },
+      },
+    },
+  ],
+  recover: [
+    {
+      psiName: "psi_lifeup",
+      psiDisplayName: "PSI Lifeup",
+      psiSubject: "allies",
+      psiScope: "one",
+      psiSubjectMessage: "To one of us",
+      psiLevels: {
+        psiLevelType: 1,
+        alpha: {
+          restore: 100,
+          ppCost: 5,
+        },
+      },
+    },
+  ],
+  assist: [],
+};
+
 hpCounter.value = PLAYER_STATS.hp;
 ppCounter.value = PLAYER_STATS.pp;
 
 actionButtons.bash.children[0].addEventListener("click", () => {
   displayCharacterSelector("enemies", actionButtons.bash, Enemies);
 });
-document.getElementById("action_psi").addEventListener("click", async () => {
-  ppCounter.rollTo(0, 40, 10);
-  document.querySelector(".actions__inner").classList.add("hidden");
-  document.querySelector(".dialogue-box").classList.remove("hidden");
-  await typewrite(
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    30
-  );
+actionButtons.psi.children[0].addEventListener("click", () => {
+  displayPsiSelector(actionButtons.psi);
 });
 
 document.addEventListener("DOMContentLoaded", () => {
   // Las acciones hacen ruido al hacer click en ellas
   Object.entries(actionButtons).forEach((key) => {
-    key[1].addEventListener("click", () => {
+    key[1].addEventListener("click", (e) => {
+      // e.stopPropagation();
+      mainUiOptionSelected = key[1];
       SFX.text.play();
     });
   });
